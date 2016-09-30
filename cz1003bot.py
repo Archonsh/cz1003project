f12e34fe571d83388e2a02fbb6c38c2cf43e3a62
BB <wubian27@yahoo.com.sg>
2016-09-30 11:40:52 +0800

    comments

--- cz1003bot.py
+++ cz1003bot.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 # _*_ coding:utf8 _*_
 
+# import necessary libraries and functions
 import telepot
 import telepot.aio
 import time
@@ -11,19 +12,26 @@ from lib_func import lib_keyboard,lib_keyboard_select,nearest_lib,lib_close,lib_
 from data_func import html,extract,ma_A,ma_B
 
 
+# ================= library list init =======================
+lib=['LWN Lib','HSS Lib','BIZ Lib','ADM Lib','CHN Lib']		# 'Lib' for 'Library Current Status Inquiry'
+lib_p=['LWN lib','HSS lib','BIZ lib','ADM lib','CHN lib']	# 'lib' for 'Library Status Prediction'
 
-lib=['LWN Lib','HSS Lib','BIZ Lib','ADM Lib','CHN Lib']
-lib_p=['LWN lib','HSS lib','BIZ lib','ADM lib','CHN lib']
 
+# =========== function to handle on-chat message ============
 async def on_chat_message(msg):
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	print(content_type, chat_type, chat_id)
 
+	# ====== check if lib is closed ======
 	if lib_close(chat_id):
 			return
 
+	# ====== check the content type ======
+	# only when users ask for nearest lib, the content type will be 'lcoation'
+	# otherwise, it will be 'text'
 	if content_type=='text':
-		
+		# === after service selection ===
+		# select lib
 		if msg['text']=='Library Current Status Inquiry':
 			# await asyncio.sleep(10)
 			await bot.sendMessage(chat_id, 'Please Choose Your Library', reply_markup=lib_keyboard())
@@ -31,27 +39,44 @@ async def on_chat_message(msg):
 		elif msg['text']=='Library Status Prediction':
 		  	await bot.sendMessage(chat_id, 'Where do you want to go?', reply_markup=lib_p_keyboard())
 
-		elif msg['text'] in lib:
+		# ===== after lib selection =====
+		# select area
+		elif msg['text'] in lib:		# for lib current status inquiry
 			n=lib.index(msg['text'])
 			await bot.sendMessage(chat_id, 'Please Choose Your Floor and Section', reply_markup=lib_keyboard_select(n))
 
-		elif msg['text'] in lib_p:
+		elif msg['text'] in lib_p:		# for lib status prediction
 			n=lib_p.index(msg['text'])
 			await bot.sendMessage(chat_id, 'Please Choose Your Floor and Section', reply_markup=lib_p_keyboard_select(n))
 
+		# ====== start / restart ======
+		# any other text sent by users will start/restart the chat session
 		else:
 			await bot.sendMessage(chat_id, 'Welcome to NTU library assistant,\nwhat can I do for you?', reply_markup=welcome_keyboard())
 
+	# ======= ask for nearest lib ========
 	if content_type=='location':
 		n=nearest_lib(msg['location']['latitude'],msg['location']['longitude'])
 		print(n)
 		await bot.sendMessage(chat_id, 'The nearest lib is '+lib[n]+'rary.'+'\nChoose your destination', reply_markup=lib_keyboard_select(n))
 
 
+# ========== function to handle callback queries ==========
 async def on_callback_query(msg):
 	query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
 	print('Callback Query:', query_id, from_id, query_data)
+
+	# display waiting information
 	await bot.answerCallbackQuery(query_id, text='Got it, just a moment...')
+
+	# distinguish the callback queries by the length of callback_data
+	# i.e. 3 for "Library Current Status Inquiry"
+	#	4 for "Library Status Prediction"
+	# see more in 'lib_func.py'
+
+	# ===== reply for "Library Current Status Inquiry" =====
+	# use lib_status() to tell the number of available seats
+	# see more in 'lib_func.py'
 	if len(query_data)==3:
 		if query_data=='lwn':
 			await bot.sendMessage(from_id,text=lib_status(0))
@@ -63,25 +88,32 @@ async def on_callback_query(msg):
 			await bot.sendMessage(from_id,text=lib_status(1))
 		elif query_data=='chn':
 			await bot.sendMessage(from_id,text=lib_status(1))
+
+	# ======= reply for "Library Status Prediction" ========
+	# use ma_A() & ma_B() to predict the estimated number of people
+	# see more in 'data_func.py'
 	else:
 		if query_data=='lwnp':
-			await bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_A(12)) 
+			await bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_A(12))
 		elif query_data=='hssp':
-			await bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_A(12)) 
+			await bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_A(12))
 		elif query_data=='bizp':
-			await bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_A(12)) 
+			await bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_A(12))
 		elif query_data=='admp':
-			await bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_B(12)) 
+			await bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_B(12))
 		elif query_data=='chnp':
-			await bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_B(12)) 
+			await bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_B(12))
+
+# ============== function to update database ====================
 async def database_update():
 	while True:
 		await html()
-		await asyncio.sleep(300)
+		await asyncio.sleep(300)	# update every 300 seconds
 
+# =================== to keep the bot running ====================
 bot = telepot.aio.Bot('194233496:AAFTNsUhxGYgb1ij84x0bys3Zhv3CNnE81w')
-loop=asyncio.get_event_loop()
-tasks=[bot.message_loop({'chat':on_chat_message,'callback_query':on_callback_query}),database_update()]
+loop = asyncio.get_event_loop()
+tasks = [bot.message_loop({'chat':on_chat_message,'callback_query':on_callback_query}),database_update()]
 loop.create_task(asyncio.wait(tasks))
 
 print ('Listening ...')
--- data_func.py
+++ data_func.py
@@ -5,57 +5,123 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
+# ======================= crawler ============================
 def html():
+    # === inicialization ===
+
+    # url init
     main_url = ['http://ipsmold.wicp.io:81/' ,'http://103.244.103.154/']
     login_url =['http://ipsmold.wicp.io:81/login/x', 'http://103.244.103.154/login/x']
     url = ['http://ipsmold.wicp.io:81/Network/ac_user',"http://103.244.103.154/Network/ac_user"]
-    payload={'user':'temp','pass':'4&4+?rpw%akU!AKG'}
+
+    # username and password to be pass into the http POST method
+    payload = {'user':'temp','pass':'4&4+?rpw%akU!AKG'}
+
+    # open a session to maintain the cookie and other information
     s = requests.Session()
+
+    # number array init
     number = []
+
+    # === get the data from url ===
+    # loop for the two different AP
     for i in range(len(url)):
+
+        # access the main url
         s.get(main_url[i])
+
+        # post uername and password to login
         s.post(login_url[i],data=payload)
+
+        # get the return content
         r = s.get(url[i])
+
+        # parse html to extract necessary data
         html_markup = BeautifulSoup(r.text,'html.parser')
         number.append(len(html_markup.find_all('dd')) - 1)
 
+    # === create database ===
+    # file init
     with open('database','r+') as f:
+
+        # read the content
         old = f.read()
+
+        # go to the 1st byte in the file
         f.seek(0)
+
+        # timestamp for keeping track
         new_data = time.strftime("%m-%d,%H:%M:%S",time.localtime())+','
     #print(time.strftime("%m-%d,%H:%M:%S",time.gmtime()),file=data,end=',')
+
+        # loop to detemine where to insert , or newline
         for i in range(len(number)):
             if i == 0:
-                new_data = new_data + str(number[i]) + ',' 
+                new_data = new_data + str(number[i]) + ','
             elif i == 1:
                 new_data = new_data + str(number[i]) + '\n'
-        f.write(new_data + old)     
-    
+
+        # add new data to the database
+        f.write(new_data + old)
+
     return number
 
+# ========== function to extract data from database ==========
 def extract(n):
+    # === inicialization ===
+    # return array init
     line = []
+
+    # counter init
     count = n
+
+    # file init
     data = open('database','r')
+
+    # === extraction ===
     for i in data:
+
+        # exit information
         if n < 1 :
             break
+
+        # get one number
         tmp = i.replace('\n','').split(',')
+
+        # add it to line[]
         line.append(tmp)
+
+        # counter updating
         n-=1
     return line
 
+# ======== function to estimate the nubmer of people =========
+# estimate by calculating the weighed average over a previous time period
+# data near the request time have larger weights
+
+# data set A
 def ma_A(periods):
+
+    # get a data array with length periods from the database
     data = extract(periods)
+
+    # weight & average init
     weight,avg=periods,0
+
+    # loop to sum up the weighed data
     for i in data:
         avg+=(int(i[3])*weight)
         weight-=1
         if weight == 0:
             break
+
+    # weighed average = (weighed sum) / (sum of weights)
+    # sum of weights = 1 + 2 +3 + ... + periods = (periods * (periods +1) / 2)
     avg= 2 * avg / (periods*(periods+1))
     return int(avg)
 
+
+# data set B (same calculation)
 def ma_B(periods):
     data = extract(periods)
     weight,avg=periods,0
